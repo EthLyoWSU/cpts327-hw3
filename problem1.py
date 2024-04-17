8b42c441669db7bc3cc37a6962551d160a7705b0
Ethan Lyons <ethan.j.lyons@wsu.edu>
2024-04-17 04:14:23 -0700

    Attempting to finish problem rough

--- problem1.py
+++ problem1.py
@@ -24,7 +24,37 @@ def findintervalcorrect(process):
     # The difference between the time it takes to calculate each two is the time difference between
     # a correct bit and an incorrect bit.
     return abs(time0 - time1)
-    
+
+def findanswer(process, differencetime, currentknown, time):
+    # We don't know how long wrong or right takes. But we know that wrong is always faster.
+    # If this is the first time through, then we need to establish a time.
+    if (len(differencetime = 0)):
+        # Then we need to check both and determine which is longer.
+        time0 = sendtostrangebitcheck(b"0"+b"0"*31, process)
+        time1 = sendtostrangebitcheck(b"1"+b"0"*31, process)
+        if time0 > time1:
+            return findanswer(process, differencetime, b"0", time0)
+        else:
+            return findanswer(process, differencetime, b"1", time1)
+    else:         
+        # Create our input to test the timing of.
+        input = currentknown+b"1"+b"0"*(31-len(currentknown))
+        # Test the timing
+        timenew = sendtostrangebitcheck(input, process)
+        # If the time is not -1
+        #   If the time is greater than the time of the previous time:
+        #       Then 1 is correct in this bit place, recursive call
+        #   Else
+        #       Then 0 is correct in this bit place, recursive call
+        # Else
+        #   The current input is the key.
+        if (timenew > 0):
+            if (timenew > time):
+                return findanswer(process, differencetime, currentknown+b"1", timenew)
+            else:
+                return findanswer(process, differencetime, currentknown+b"0", timenew)
+        else:
+            return input
 
 # Starting StrangeSystem
 beginCommand = '/home/reverbfortuna/Documents/cpts327 hw3/StrangeSystem'
@@ -36,6 +66,10 @@ print (proc.recvuntil("Enter your 32-bits PIN code (in binary form like PIN in p
 
 # Work goes here
 interval = findintervalcorrect(proc)
+output = findanswer(process, interval, b"", 0)
+print("\n\n" + str(output) + "\n\n")
+
+proc.interactive()
 
 # Exiting
 proc.close()
