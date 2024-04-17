93318122ece2269f82f2e5fb4e4326d3f2a8d846
Ethan Lyons <ethan.j.lyons@wsu.edu>
2024-04-17 03:39:51 -0700

    Am now able to determine the time difference between a correct and incorrect bit

--- problem1.py
+++ problem1.py
@@ -1,20 +1,29 @@
 from pwn import *
 from time import *
 
+# This code checks a particular bit input. If it is not correct, it returns the time it took, otherwise -1
+def sendtostrangebitcheck(input, process):
+    process.clean()
+    start = time()
+    process.sendline(input)
+    output = process.recvuntil("ERROR: Verification failed!", False, timeout=60)
+    process.clean()
+    process.sendline(b"YES")
+    end = time()
+    if output == '':
+        return -1
+    else:
+        return end - start
+
 # This code is going to be used to find time intervals for this process.
 # This can be found by checking two numbers, 00^(n-1) and 10^(n-1) where n is the length.
 def findintervalcorrect(process):
     process.clean()
-    start0 = time.time()
-    process.sendline(b"0"*32)
-    print (process.recvuntil("ERROR: Verification failed!"))
-    end0 = time.time()
-    process.clean()
-    start1 = time.time()
-    process.sendline(b"1"+b"0"*31)
-    print (process.recvuntil("ERROR: Verification failed!"))
-    end1 = time.time()
-    return abs((end0 - start0) - (end1 - start1))
+    time0 = sendtostrangebitcheck(b"0"+b"0"*31, process)
+    time1 = sendtostrangebitcheck(b"1"+b"0"*31, process)
+    # The difference between the time it takes to calculate each two is the time difference between
+    # a correct bit and an incorrect bit.
+    return abs(time0 - time1)
     
 
 # Starting StrangeSystem
