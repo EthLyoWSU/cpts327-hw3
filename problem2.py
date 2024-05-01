94e99aaf958b06113062c8a9c63fa4dd2ae09530
Ethan Lyons <ethan.j.lyons@wsu.edu>
2024-04-30 20:41:39 -0700

    Implemented code to find matching potential a and b for ga and gb from G.

--- problem2.py
+++ problem2.py
@@ -5,10 +5,25 @@ q = 509
 P = 1019
 g = 3
 
-set_dictionary = {}
+setList = []
+
+def returniList(gIn, G):
+    matchingList = []
+    for i in range(0, P):
+        if G[i] == gIn:
+            matchingList.append(i)
+    print(str(matchingList))
+    return matchingList
 
 # Code generates entire possible G
 for i in range(0, P):
-    set_dictionary[i] = pow(g, i, P)
+    setList.append( pow(g, i, P))
 
-print(str(set_dictionary))
+print(str(setList))
+
+# Now that we have the list of G, we can narrow down to two lists.
+# These lists will contain all of the i for which gi = ga or gb.
+print("Finding Matching list for ga")
+gamatch = returniList(p2ga, setList)
+print("Finding Matching list for gb")
+gamatch = returniList(p2gb, setList)
