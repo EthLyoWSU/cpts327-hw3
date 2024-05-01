800fb10ef057991e02ffca31151c3de093ae8e3d
Ethan Lyons <ethan.j.lyons@wsu.edu>
2024-04-30 21:36:42 -0700

    Found gab, a, b. Fixed error in code where range was P instead of q.

--- problem2.py
+++ problem2.py
@@ -7,16 +7,17 @@ g = 3
 
 setList = []
 
+# The following code takes a g value and G before returning an array of possible i values where gi = g
 def returniList(gIn, G):
     matchingList = []
-    for i in range(0, P):
+    for i in range(0, q):
         if G[i] == gIn:
             matchingList.append(i)
     print(str(matchingList))
     return matchingList
 
-# Code generates entire possible G
-for i in range(0, P):
+# Code generates entire possible G, index is i
+for i in range(0, q):
     setList.append( pow(g, i, P))
 
 print(str(setList))
@@ -26,4 +27,14 @@ print(str(setList))
 print("Finding Matching list for ga")
 gamatch = returniList(p2ga, setList)
 print("Finding Matching list for gb")
-gamatch = returniList(p2gb, setList)
+gbmatch = returniList(p2gb, setList)
+
+# Checking if (g^a)^b mod P and (g^b)^a mod P are the same. If they are we know a, b, and gab
+for a in gamatch:
+    for b in gbmatch:
+        gabfromga = pow(pow(g, b), a, P)
+        gabfromgb = pow(pow(g, a), b, P)
+        if gabfromga == gabfromgb:
+            print("Match found! gab = " + str(gabfromga) + " a = " + str(a) + " b = " + str(b))
+
+# gab is 211, a is 106, and b is 356
